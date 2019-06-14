c906857a4b7ee84c8e3c9f0f6b69e5a2d9ec453f
wouterlansu <wfrlansu@gmail.com>
2019-06-14 23:16:07 +0200

    Some fine tuning, renamed some variables to better expose what they are.

--- src/com/crawler/Crawler.java
+++ src/com/crawler/Crawler.java
@@ -13,31 +13,32 @@ import org.jsoup.select.Elements;
 
 
 /**
- * Main class for the webcloud app.
+ * Main class for the crawler app.
  *
  * <p>
- *     Spider crawls a specific hardcoded url and further crawls all urls found on that page to detect the
- *     most frequently used words. These words are added to a HashMap so we can see what words are most frequent per url.
+ *     Crawl a specific hardcoded url and further crawl all urls found on that page to detect the most
+ *     frequently used words. These words are added to a HashMap so we can see what words are most frequent per url.
  * </p>
  */
 public class Crawler {
 
-    private Map<String, Map<String, Integer>> visited = new HashMap<>();
+    private Map<String, Map<String, Integer>> crawled = new HashMap<>();
     private String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:60.0) Gecko/20100101 Firefox/60.0";
 
     /**
      * Main method.
      *
      * <p>
-     *     Calls the crawl method to do most of the work and then prints out the results of the crawl.
+     *     Call the crawl method to do most of the work and then print out the results of the crawl.
      * </p>
+     *
      * @param args None
      */
     public static void main(String[] args) {
         Crawler crawler = new Crawler();
         long start = System.currentTimeMillis();
         crawler.crawl();
-        for (Object url : crawler.visited.entrySet()) {
+        for (Object url : crawler.crawled.entrySet()) {
             Map.Entry me = (Map.Entry) url;
             System.out.print(me.getKey() + ": ");
             System.out.println(me.getValue());
@@ -45,15 +46,15 @@ public class Crawler {
         long end = System.currentTimeMillis();
         float sec = (end - start) / 1000F;
         System.out.printf(
-            "Crawled %s urls in %s seconds", crawler.visited.size(), sec
+            "Crawled %s urls in %s seconds", crawler.crawled.size(), sec
         );
     }
 
     /**
-     * Crawl a hardcoded url and calls the mostFrequentWords method on all urls found on that page.
+     * Crawl a hardcoded url and call the mostFrequentWords method on all urls found on that page.
      *
      * <p>
-     *     There is a timelimit of 60 seconds, in combination with the printing of the values in the main method
+     *     There is a time limit of 60 seconds, in combination with the printing of the values in the main method
      *     we will run a little over that.
      * </p>
      */
@@ -70,7 +71,7 @@ public class Crawler {
             System.out.printf("Found %d urls to crawl.", urlsToCrawl.size());
             long startTime = System.currentTimeMillis();
             for (Element url : urlsToCrawl) {
-                if ((System.currentTimeMillis() - startTime) < 60000) {
+                if ((System.currentTimeMillis() - startTime) < 100) {
                     this.mostFrequentWords(url.absUrl("href"));
                 } else {
                     break;
@@ -82,13 +83,13 @@ public class Crawler {
     }
 
     /**
-     * Finds the most frequently used words on a webpage and adds them to the visited HashMap.
+     * Find the most frequently used words on a webpage and add them to the crawled HashMap.
      *
      * <p>
-     *     Uses a replace and regex to parse the text on the webpage into something approaching readable text.
+     *     Use a replace and regex to parse the text on the webpage into something approaching readable text.
      *     This still needs improving though.
      *
-     *     Adds a word to the countMap when first found in the text and increments the value if found again.
+     *     Add a word to the countMap when first found in the text and increment the value if found again.
      * </p>
      *
      * @param url The url to visit.
@@ -102,9 +103,10 @@ public class Crawler {
                 .userAgent(USER_AGENT)
                 .referrer("http://www.google.com");
             String text = connection.get().body().text();
+            System.out.println(text);
             String[] words = text
                 .replaceAll("\\r\\n. |\\r. |\\n", " ")
-                .split("(?<!\\S)[A-Za-z]+(?!\\S)");  // TODO: regex needs tuning.
+                .split("(?<!\\S)\\p{L}+(?!\\S)");  // TODO: regex needs tuning.
             for (String word : words) {
                 if (countMap.containsKey(word)) {
                     countMap.replace(word, countMap.get(word) + 1);  // TODO: there is probably a better way to do this.
@@ -114,7 +116,7 @@ public class Crawler {
             }
             // TODO: This sorts by keys, apparently I could use Guava to sort by values.
             Map<String, Integer> sorted = new TreeMap<>(countMap);
-            this.visited.put(url, sorted);
+            this.crawled.put(url, sorted);
         } catch (HttpStatusException | UnsupportedMimeTypeException e) {
             // This is fine ... ;)
         }
